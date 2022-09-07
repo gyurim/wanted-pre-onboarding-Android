5032908e23972185874fe2316d4c415794d732bb
gyurim <gyurim1250@gmail.com>
2022-09-07 21:01:10 +0900

    Modify BottomNavigationView Icon - 아이콘 수정 및 Padding 적용

--- NewsApplication/app/src/main/java/com/codelab/newsapplication/ui/MainActivity.kt
+++ NewsApplication/app/src/main/java/com/codelab/newsapplication/ui/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 //        BuildConfig.DEBUG
         val mainViewModel : MainViewModel by viewModels()
         binding.lifecycleOwner = this
